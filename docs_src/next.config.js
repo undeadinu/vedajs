c84b2b50188fe996207aa03e32424bb79533f3f0
fand <fand.gmork@gmail.com>
2017-10-23 14:51:47 +0900

    Add docs

--- docs_src/next.config.js
+++ docs_src/next.config.js
@@ -1,6 +1,17 @@
 
 module.exports = {
-  exportPathMap: function() {
+  webpack(config) {
+    // Workaround for build error caused by UglifyJs.
+    // See https://github.com/zeit/next.js/issues/1253
+    config.plugins = config.plugins.filter(plugin => {
+      if (plugin.constructor.name === 'UglifyJsPlugin') {
+        return false;
+      }
+      return true;
+    });
+    return config;
+  },
+  exportPathMap() {
     return {
       '/': { page: '/' },
     };
